de43bf8bb05008776821d812a1cd0a4d60a0ec6e
konstantinskorin <52405326+konstantinskorin@users.noreply.github.com>
2019-07-15 10:46:56 +0300

    Update Project.java

--- src/main/java/com/test/Project.java
+++ src/main/java/com/test/Project.java
@@ -11,8 +11,7 @@ class Project {
 
   public static void test() {
     System.out.println("test");
-   System.out.println("test");
-   System.out.println("test");
+   
   }
 
   public static void main(String[] args) {
